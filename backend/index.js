9a4339c20f143939c49827c0f35306ab8c5c945e
bhunt222 <bhunt2018@gmail.com>
2023-12-19 22:32:35 -0500

    MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var cors = require('cors');
 var path = require('path');
-// var projectDBRouter = require('./routes/projectDB');
+var projectDBRouter = require('./routes/projectDB');
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/sampleAPI');
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/projectDB', projectDBRouter);
+app.use('/projectDB', projectDBRouter);
 app.use('/', indexRouter);
 app.use('/sampleAPI', apiRouter);
 
